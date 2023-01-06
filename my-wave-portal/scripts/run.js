b2d42a278ff71ae6c214ca1c4b9abe91547deb6c
HGani <hgani786@gmail.com>
2023-01-06 11:54:09 +0200

    Store data on the smart contract

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,8 +1,23 @@
-const main = async () => {
+  const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+  
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+  
+    await waveContract.getTotalWaves();
+  
+    const firstWaveTxn = await waveContract.wave();
+    await firstWaveTxn.wait();
+
+    await waveContract.getTotalWaves();
+
+    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+    await secondWaveTxn.wait();
+  
+    await waveContract.getTotalWaves();
   };
   
   const runMain = async () => {
@@ -13,7 +28,6 @@ const main = async () => {
       console.log(error);
       process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
     }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
   };
   
   runMain();
